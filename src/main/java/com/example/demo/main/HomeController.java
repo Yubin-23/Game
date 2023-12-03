7c30fc678971ad2a2a4499b70f23d10847f65655
pcy5653 <pcy7625653@naver.com>
2023-12-03 16:54:02 +0900

    game_1

---
+++ src/main/java/com/example/demo/main/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping("/")
+	public String getHome()throws Exception{
+		return "/home";
+	}
+}
